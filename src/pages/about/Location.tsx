03bdd17377eed7d689002290c64ba1102e14d11a
mxten777 <mxten777@gmail.com>
2025-12-25 07:11:06 +0900

    fix: prevent scroll jump caused by iframe load

--- src/pages/about/Location.tsx
+++ src/pages/about/Location.tsx
@@ -103,7 +103,7 @@ const Location = () => {
             </div>
 
             <div className="bg-white dark:bg-neutral-800 rounded-3xl shadow-2xl overflow-hidden border border-neutral-200 dark:border-neutral-700">
-              <div className="aspect-[16/9] relative">
+              <div className="aspect-[16/9] relative min-h-[360px]">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.234!2d127.4894!3d36.5674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzbCsDM0JzAyLjciTiAxMjfCsDI5JzIxLjgiRQ!5e0!3m2!1sko!2skr!4v1234567890"
                   width="100%"
